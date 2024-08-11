49f154609864a568d3748b5ccc18506ec9947b84
Daya Sagar <152199702+Dayasagar88@users.noreply.github.com>
2024-08-11 18:16:37 +0530

    bug-fix

--- db.js
+++ db.js
@@ -7,8 +7,8 @@ const mongoURL = process.env.MONGODB_URL;
 
 //Setup mongo DB connection
 mongoose.connect(mongoURL, {
-    // useNewUrlParser: true,
-    // useUnifiedTopology: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 })
 
 //Get a default connection
